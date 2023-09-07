d0e2028954264416c670c1933bec174e52c1dc9e
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2023-09-07 18:30:41 +0900

    aws

--- main.tf
+++ main.tf
@@ -1,27 +1,23 @@
 terraform {
   required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0.1"
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
     }
   }
+
+  required_version = ">= 1.2.0"
 }
 
-provider "docker" {
-  host = "unix:///Users/takaha4k/.docker/run/docker.sock"
+provider "aws" {
+  region  = "us-west-2"
 }
 
-resource "docker_image" "nginx" {
-  name         = "nginx"
-  keep_locally = false
-}
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = "tutorial"
-
-  ports {
-    internal = 80
-    external = 8000
+  tags = {
+    Name = "ExampleAppServerInstance"
   }
 }
