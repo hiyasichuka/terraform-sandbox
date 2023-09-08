3a7f44df3ed9afbff5ddc525f2cba1a97c16cbba
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2023-09-08 14:36:23 +0900

    Update AWS region and AMI for the app server resource

--- main.tf
+++ main.tf
@@ -10,11 +10,11 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-northeast-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0bba69335379e17f8"
   instance_type = "t2.micro"
 
   tags = {
