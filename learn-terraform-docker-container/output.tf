b39f7eb1370b2f4eea3c1519b0e5388068e701a2
hiyasichuka <hiyasichukahajimemasita@gmail.com>
2024-02-11 18:19:17 +0900

    Add output docker image name

--- learn-terraform-docker-container/output.tf
+++ learn-terraform-docker-container/output.tf
@@ -7,3 +7,8 @@ output "image_id" {
   description = "ID of the Docker image"
   value       = docker_image.nginx.id
 }
+
+output "image_name" {
+  description = "Name of the Docker image"
+  value = docker_image.nginx.name
+}
