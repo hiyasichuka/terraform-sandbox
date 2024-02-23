e1a569487790757facd1bee1f18ce160055032a8
takahashi-kei <takahashi-kei@dmm.com>
2024-02-23 18:36:14 +0900

    Add OpenID resources

---
+++ bq-omni/openid.tf
@@ -0,0 +1,9 @@
+resource "aws_iam_openid_connect_provider" "default" {
+  url = "https://accounts.google.com"
+
+  client_id_list = [
+    google_bigquery_connection.connection.aws[0].access_role[0].identity,
+  ]
+
+  thumbprint_list = ["cf23df2207d99a74fbe169e3eba035e633b65d94"]
+}
