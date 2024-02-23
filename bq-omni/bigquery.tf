040afc340c49e20ab8561e8845dd1dfb37549cd2
takahashi-kei <takahashi-kei@dmm.com>
2024-02-23 18:03:12 +0900

    Add bigquery table

--- bq-omni/bigquery.tf
+++ bq-omni/bigquery.tf
@@ -4,23 +4,26 @@ resource "google_bigquery_dataset" "bigquery_dataset" {
   location    = "aws-${var.aws_region}"
 }
 
-#resource "google_bigquery_table" "external_table" {
-#  project  = var.gcp_project_id
-#  dataset_id = google_bigquery_dataset.bigquery_dataset.dataset_id
-#  table_id = "csv"
-#
-#  external_data_configuration {
-#    autodetect    = true
-#    source_format = "CSV"
-#    source_uris   = ["s3://${aws_s3_bucket.bucket.id}/*.csv"]
-#    csv_options {
-#      quote = ""
-#      skip_leading_rows = 1
-#    }
-#  }
-#
-#  depends_on = [
-#    google_bigquery_dataset.bigquery_dataset,
-#    aws_s3_bucket.bucket
-#  ]
-#}
+resource "google_bigquery_table" "external_table" {
+  project  = var.gcp_project_id
+  dataset_id = google_bigquery_dataset.bigquery_dataset.dataset_id
+  table_id = "sample_table"
+  deletion_protection=false
+
+  external_data_configuration {
+    autodetect    = true
+    connection_id = google_bigquery_connection.connection.id
+    source_format = "CSV"
+    source_uris   = ["s3://${aws_s3_bucket.bucket.id}/*.csv"]
+    csv_options {
+      quote = "\""
+      skip_leading_rows = 1
+      allow_quoted_newlines = true
+    }
+  }
+
+  depends_on = [
+    google_bigquery_dataset.bigquery_dataset,
+    aws_s3_bucket.bucket
+  ]
+}
