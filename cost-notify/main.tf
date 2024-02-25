eb5dfe1ded323bbd4a7b1eabfae87ffebad60a04
takahashi-kei <takahashi-kei@dmm.com>
2024-02-25 14:43:11 +0900

    add functions

--- cost-notify/main.tf
+++ cost-notify/main.tf
@@ -1,24 +1,56 @@
+# Terraform setting
+terraform {
+  required_version = "~> 1.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Provider
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   region     = var.aws_region
 }
 
-data "archive_file" "cost_notifier_zip" {
+# Archive
+data "archive_file" "layer_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/lambda_function"
-  output_path = "${path.module}/cost_notifier.zip"
+  source_dir  = "build/layer"
+  output_path = "lambda/layer.zip"
+}
+data "archive_file" "function_zip" {
+  type        = "zip"
+  source_dir  = "build/function"
+  output_path = "lambda/function.zip"
 }
 
+# Layer
+resource "aws_lambda_layer_version" "lambda_layer" {
+  layer_name          = "lambda_layer"
+  compatible_runtimes = ["python3.10"]
+  filename            = data.archive_file.layer_zip.output_path
+  source_code_hash    = filebase64sha256(data.archive_file.layer_zip.output_path)
+}
+
+# Function
 resource "aws_lambda_function" "cost_notifier" {
   function_name    = "cost_notifier"
-  handler          = "main.lambda_handler"
+  handler          = "src/cost_notifier.lambda_handler"
   runtime          = "python3.10"
-  filename         = data.archive_file.cost_notifier_zip.output_path
-  source_code_hash = filebase64sha256(data.archive_file.cost_notifier_zip.output_path)
+  filename         = data.archive_file.function_zip.output_path
   role             = aws_iam_role.lambda_exec.arn
+  source_code_hash = filebase64sha256(data.archive_file.function_zip.output_path)
+  layers           = [aws_lambda_layer_version.lambda_layer.arn]
+  memory_size      = 128
+  timeout          = 30
 }
 
+# Role
 resource "aws_iam_role" "lambda_exec" {
   name = "lambda-exec-role"
 
@@ -39,6 +71,7 @@ resource "aws_iam_role" "lambda_exec" {
 EOF
 }
 
+# Policy
 resource "aws_iam_policy" "lambda_policy" {
   name        = "lambda-policy"
   description = "IAM policy for the Lambda function"
