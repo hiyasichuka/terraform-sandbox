586f58d1f33e25f2ad5df95445bbf7a3d2f7be5d
takahashi-kei <takahashi-kei@dmm.com>
2024-02-27 19:14:32 +0900

    add datasource and remove table

--- bq-omni/bigquery.tf
+++ bq-omni/bigquery.tf
@@ -5,27 +5,28 @@ resource "google_bigquery_dataset" "bigquery_dataset" {
   delete_contents_on_destroy = true
 }
 
-resource "google_bigquery_table" "external_table" {
-  project             = var.gcp_project_id
-  dataset_id          = google_bigquery_dataset.bigquery_dataset.dataset_id
-  table_id            = "sample_table"
-  deletion_protection = false
-
-  external_data_configuration {
-    autodetect    = true
-    connection_id = google_bigquery_connection.aws_connection.id
-    source_format = "CSV"
-    source_uris   = ["s3://${aws_s3_bucket.bucket.id}/*"]
-    csv_options {
-      quote                 = "\""
-      skip_leading_rows     = 1
-      allow_quoted_newlines = true
-    }
-  }
-
-  depends_on = [
-    google_bigquery_dataset.bigquery_dataset,
-    aws_s3_bucket.bucket,
-    google_bigquery_connection.aws_connection,
-  ]
+resource "google_bigquery_dataset" "bigquery_dataset_US" {
+  dataset_id                 = "dataset"
+  description                = "Test data source"
+  location                   = "US"
+  delete_contents_on_destroy = true
 }
+
+#resource "google_bigquery_table" "external_table" {
+#  project             = var.gcp_project_id
+#  dataset_id          = google_bigquery_dataset.bigquery_dataset.dataset_id
+#  table_id            = "sample_table"
+#  deletion_protection = false
+#
+#  external_data_configuration {
+#    autodetect    = true
+#    connection_id = google_bigquery_connection.aws_connection.id
+#    source_format = "CSV"
+#    source_uris   = ["s3://${aws_s3_bucket.bucket.id}/*"]
+#    csv_options {
+#      quote                 = "\""
+#      skip_leading_rows     = 1
+#      allow_quoted_newlines = true
+#    }
+#  }
+#}
