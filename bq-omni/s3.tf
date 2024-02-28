7d4a03a05a8d6282b14e5686716ceb54ccb830db
takahashi-kei <takahashi-kei@dmm.com>
2024-02-28 16:10:41 +0900

    remove cors

--- bq-omni/s3.tf
+++ bq-omni/s3.tf
@@ -3,14 +3,14 @@ resource "aws_s3_bucket" "bucket" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_cors_configuration" "cors" {
-  bucket = aws_s3_bucket.bucket.id
-
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["PUT", "POST"]
-    allowed_origins = ["*"]
-    expose_headers  = ["ETag"]
-    max_age_seconds = 3000
-  }
-}
+#resource "aws_s3_bucket_cors_configuration" "cors" {
+#  bucket = aws_s3_bucket.bucket.id
+#
+#  cors_rule {
+#    allowed_headers = ["*"]
+#    allowed_methods = ["PUT", "POST"]
+#    allowed_origins = ["*"]
+#    expose_headers  = ["ETag"]
+#    max_age_seconds = 3000
+#  }
+#}
